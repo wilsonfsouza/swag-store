d96ddb47ad1eee18365355424e1c30389f508437
Wilson Franca <wilson.franca.92@gmail.com>
2025-11-29 15:43:06 -0800

    fix(layout): update font family to be inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const geistSans = Inter({
+  variable: '--font-inter',
   subsets: ['latin'],
 });
 
@@ -24,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
     </html>
   );
 }
