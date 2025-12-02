c8bb060ea06978fde88ac733ab43dbcd536c4501
Wilson Franca <wilson.franca.92@gmail.com>
2025-12-01 21:37:32 -0800

    fix(data): added /api as API prefix

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,7 +2,8 @@ import { env } from '@/config/env';
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL;
-  const url = new URL(path, baseUrl);
+  const API_PREFIX = '/api';
+  const url = new URL(API_PREFIX.concat(path), baseUrl);
 
   return fetch(url, init);
 }
