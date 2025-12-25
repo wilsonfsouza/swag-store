11b9bc177f249edf41dd9cec65d98f9806f382c8
Wilson Franca <wilson.franca.92@gmail.com>
2025-12-24 18:18:13 -0800

    feat: add context/custom hook for cart

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,13 +1,16 @@
 import { Header } from '@/components/header';
+import { CartProvider } from '@/contexts/cart-context';
 
 interface StoreLayoutProps {
   children: React.ReactNode;
 }
 export default function StoreLayout({ children }: StoreLayoutProps) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   );
 }
